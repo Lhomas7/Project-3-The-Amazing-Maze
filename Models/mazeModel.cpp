146a28e009d5961c0615b63939d23ac4a02abacf
Benjamin Newton <newton-03@hotmail.com>
2025-11-18 21:37:05 -0500

    progressed all vertical walls (left/right) neighbors

--- Models/mazeModel.cpp
+++ Models/mazeModel.cpp
@@ -1,6 +1,6 @@
 #include "mazeModel.h"
 #include <algorithm>
-
+#include <cstdlib> // for rand() 
 
 
 
@@ -18,11 +18,24 @@ MazeModel::MazeModel(int rows, int cols)
       leftWalls(rows * cols, true),
       nextWallIndex(0),
       finished(false) {
+
     allPresentWalls();
     shuffleWalls();
     updateFinishedStatus();
+
       }
 
     void MazeModel::shuffleWalls(){
-        
+    walls.clear();
+
+    walls.reserve((modelRows * (modelCols - 1)) + ((modelRows - 1) * modelCols));
+
+    for (int row = 0; row < modelRows; ++row) {
+        for (int col = 0; col < modelCols -1; ++col) {
+            Wall wall;
+            wall.cell1 = index(row, col);
+            wall.cell2 = index(row, col + 1);
+            walls.push_back(wall);
+        }
+    }
     }
